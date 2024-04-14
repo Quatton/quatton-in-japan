5f6ba54aec88e6500dfd9588f7718fbe7df64a1d
Quatton <atomicatives@gmail.com>
2024-04-15 02:01:15 +0900

    basic street view

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,31 @@
+import { useJsApiLoader } from "@react-google-maps/api";
 import type { MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { StreetView } from "~/components/street-view";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Quatton in Japan" },
+    { name: "description", content: "Welcome to Japan!" },
   ];
 };
 
+export function loader() {
+  return { googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY! };
+}
+
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+  const { googleMapsApiKey } = useLoaderData<typeof loader>();
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey,
+    region: "JP",
+  });
+
+  if (!isLoaded) {
+    return null;
+  }
+
+  return <StreetView />;
 }
